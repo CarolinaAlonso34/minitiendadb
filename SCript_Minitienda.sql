2ad6659da4f3563b7c8ff4ae559718485b1752c5
Carolina Alonso <carolinaalonsog34@gmail.com>
2024-09-18 17:15:17 -0500

    Se crea el script con los sp de la MiniTienda, de la tabla Categorias

---
+++ SCript_Minitienda.sql
@@ -0,0 +1,24 @@
+-- Categorias
+-- Insertar
+DELIMITER //
+
+DELIMITER ;
+-- Actualizar
+DELIMITER //
+
+DELIMITER ;
+-- Mostrar
+DELIMITER //
+
+DELIMITER ;
+-- Eliminar
+DELIMITER //
+
+DELIMITER ;
+
+
+-- Provedores
+
+-- Productos 
+
+-- Usuarios 
